170de9a62ba0aec6c78dfee6a81d1f39215238df
peteryunkim <peteryunkim@gmail.com>
2017-09-20 14:06:55 -0400

    added second page

--- App.js
+++ App.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import CurrentMembers from './Components/CurrentMembers'
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+// export default class App extends React.Component {
+//   render() {
+//     return (
+      // <View style={styles.container}>
+      //   <Text>Welcome to Midtown West home group!</Text>
+      //   <Text>Our aim is to create a safe haven for everyone.</Text>
+      //   <Text>We will be building a tightly knit community that pursues Jesus and the Gospel.</Text>
+      //   <CurrentMembers />
+      // </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'flex-start',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'flex-start',
+//     justifyContent: 'center',
+//   },
+// });
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
   render() {
+    const { navigate } = this.props.navigation;
     return (
-      <View style={styles.container}>
+      <View >
         <Text>Welcome to Midtown West home group!</Text>
         <Text>Our aim is to create a safe haven for everyone.</Text>
         <Text>We will be building a tightly knit community that pursues Jesus and the Gospel.</Text>
-        <CurrentMembers />
+        <Button title='See members' onPress={() => navigate('SignUp')}/>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-  },
+export default SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  SignUp: {screen: CurrentMembers}
 });
+
--- Components/CurrentMembers.js
+++ Components/CurrentMembers.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, Picker, StatusBar} from 'react-native'
 import Form from './Form'
 
 export default class CurrentMembers extends React.Component{
+	static navigationOptions = {
+    title: 'Members',
+  };
+
 	constructor(){
 		super()
 		this.state={
-			members: ['Peter', 'Joyce']
+			members: ['Peter', 'Joyce'],
+			location: ""
 		}
 	}
 
@@ -20,11 +25,17 @@ export default class CurrentMembers extends React.Component{
 	render(){
 		return(
 			<View>
-			<Text>
-				Current Members:
-			</Text>
+				<Text>
+					Current Members:
+				</Text>
 				{this.state.members.map(name => <Text key={name}>{name}</Text>)}
 				<Form addMember={this.addMember}/>
+				<Picker selectedValue={this.state.location} onValueChange={item => this.setState({location: item})}>
+					<Picker.Item label='Midtown West' value='MW'/>
+					<Picker.Item label='Midtown East' value='ME'/>
+				</Picker>
+				<StatusBar>
+				</StatusBar>
 			</View>
 		)
 	}
