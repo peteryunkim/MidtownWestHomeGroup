ce3a9e8baf1ac8114e60fe3c232b3c46f1658f71
peteryunkim <peteryunkim@gmail.com>
2017-09-20 14:35:36 -0400

    added logo

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, ScrollView, View, Button, Image } from 'react-native';
 import CurrentMembers from './Components/CurrentMembers'
 import { StackNavigator } from 'react-navigation';
 
@@ -7,7 +7,17 @@ import { StackNavigator } from 'react-navigation';
 const styles = StyleSheet.create({
   buttonContainer: {
     backgroundColor: 'steelblue',
-    padding: 10
+    padding: 10,
+  },
+  appContainer: {
+    padding: 10,
+    margin: 10
+  },
+  bgImage: {
+    flex: 1,
+    width: 300,
+    height: 300,
+    resizeMode: 'cover'
   }
 })
 
@@ -18,14 +28,16 @@ class HomeScreen extends React.Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View >
+      <ScrollView style={styles.appContainer}>
+        <Image source={{uri:"http://static1.squarespace.com/static/5833460bd1758efbf2e11826/t/584ec91eb8a79bf389f059bf/1481558302488/unnamed.png?format=1000w"
+}} style={styles.bgImage}/>
         <Text>Welcome to Midtown West home group!</Text>
         <Text>Our aim is to create a safe haven for everyone.</Text>
         <Text>We will be building a tightly knit community that pursues Jesus and the Gospel.</Text>
         <View style={styles.buttonContainer}>
         <Button title='See members' onPress={() => navigate('SignUp')} color="white"/>
         </View>
-      </View>
+      </ScrollView>
     )
   }
 }
